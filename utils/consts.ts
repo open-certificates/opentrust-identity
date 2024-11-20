829eb714c8b35a0b4b262c7e510fd48275c34194
David Hayek <dhayek@charter.net>
2024-11-20 16:57:48 -0600

    added more tenant method implementations

--- utils/consts.ts
+++ utils/consts.ts
@@ -7,9 +7,15 @@
 export const DEFAULT_SERVICE_ACCOUNT_TOKEN_TTL_SECONDS = 600; // 10 minutes 
 export const DEFAULT_END_USER_TOKEN_TTL_SECONDS = 3600; // 1 hour
 export const MAX_END_USER_TOKEN_TTL_SECONDS = 43200; // 12 hours
+export const MIN_END_USER_TOKEN_TTL_SECONDS = 900; // 15 minutes
 
 
 
+// ************************************************************************** //
+// 
+//                  FILE-SYSTEM-BASED DAO CONSTANTS
+// 
+// ************************************************************************** //
 export const ROOT_TENANT_FILE = "root-tenant.json";
 export const TENANT_FILE = "tenants.json";
 export const CLIENT_FILE = "clients.json";
@@ -22,3 +28,187 @@ export const CLIENT_TENANT_SCOPE_REL_FILE = "client-tenant-scope-rel.json";
 export const LOGIN_GROUP_FILE = "login-groups.json";
 export const LOGIN_GROUP_CLIENT_REL_FILE = "login-groups-client-rel.json";
 export const GROUP_FILE = "groups.json";
+
+
+
+// ************************************************************************** //
+// 
+//                          SCOPE CONSTANTS
+// 
+// ************************************************************************** //
+// Allowed characters which can be used to define a scope name. No special characters
+// or non-ASCII characters.
+export const SCOPE_NAME_PATTERN="[A-Z,a-z,0-9_-\.]"
+
+// These are ALL of the constants that are required for managing the application.
+// Additional scope values can be created for the various services that your
+// applications provide. The scope names are fixed and cannot be redefined. 
+export const TENANT_CREATE_SCOPE="tenant.create";
+export const TENANT_UPDATE_SCOPE="tenant.update";
+export const TENANT_DELETE_SCOPE="tenant.delete";
+export const TENANT_READ_SCOPE="tenant.read";
+
+export const CLIENT_CREATE_SCOPE="client.create";
+export const CLIENT_UPDATE_SCOPE="client.update";
+export const CLIENT_DELETE_SCOPE="client.delete";
+export const CLIENT_READ_SCOPE="client.read";
+
+export const SCOPE_CREATE_SCOPE="scope.create";
+export const SCOPE_UPDATE_SCOPE="scope.update";
+export const SCOPE_DELETE_SCOPE="scope.delete";
+export const SCOPE_READ_SCOPE="scope.read";
+export const SCOPE_TENANT_ASSIGN_SCOPE="scope.tenant.assign";
+export const SCOPE_TENANT_REMOVE_SCOPE="scope.tenant.remove";
+export const SCOPE_CLIENT_ASSIGN_SCOPE="scope.client.assign"
+export const SCOPE_CLIENT_REMOVE_SCOPE="scope.client.remove";
+
+export const KEY_CREATE_SCOPE="keys.create";
+export const KEY_DELETE_SCOPE="keys.delete";
+export const KEY_READ_SCOPE="keys.read";
+
+export const RATE_LIMIT_CREATE_SCOPE="ratelimit.create";
+export const RATE_LIMIT_UPDATE_SCOPE="ratelimit.update";
+export const RATE_LIMIT_DELETE_SCOPE="ratelimit.update";
+export const RATE_LIMIT_READ_SCOPE="ratelimit.read";
+export const RATE_LIMIT_TENANT_ASSIGN_SCOPE="ratelimit.tenant.assign";
+export const RATE_LIMIT_TENANT_REMOVE_SCOPE="ratelimit.tenant.remove";
+export const RATE_LIMIT_TENANT_UPDATE_SCOPE="ratelimit.tenant.update";
+
+export const LOGIN_GROUP_CREATE_SCOPE="logingroup.create";
+export const LOGIN_GROUP_UPDATE_SCOPE="logingroup.update";
+export const LOGIN_GROUP_DELETE_SCOPE="logingroup.delete";
+export const LOGIN_GROUP_READ_SCOPE="logingroup.read";
+export const LOGIN_GROUP_CLIENT_ASSIGN_SCOPE="logingroup.client.assign";
+export const LOGIN_GROUP_CLIENT_REMOVE_SCOPE="logingroup.client.remove";
+
+export const GROUP_CREATE_SCOPE="group.create";
+export const GROUP_UPDATE_SCOPE="group.update";
+export const GROUP_DELETE_SCOPE="group.delete";
+export const GROUP_READ_SCOPE="group.read";
+export const GROUP_USER_ASSIGN_SCOPE="group.user.assign";
+export const GROUP_USER_REMOVE_SCOPE="group.user.remove";
+
+export const USER_CREATE_SCOPE="user.create";
+export const USER_UPDATE_SCOPE="user.update";
+export const USER_DELETE_SCOPE="user.delete";
+export const USER_READ_SCOPE="user.read";
+
+export const EXTERNAL_OIDC_PROVIDER_CREATE_SCOPE="externaloidcprovider.create";
+export const EXTERNAL_OIDC_PROVIDER_UPDATE_SCOPE="externaloidcprovider.update";
+export const EXTERNAL_OIDC_PROVIDER_DELETE_SCOPE="externaloidcprovider.delete";
+export const EXTERNAL_OIDC_PROVIDER_READ_SCOPE="externaloidcprovider.read";
+export const EXTERNAL_OIDC_PROVIDER_TENANT_ASSIGN_SCOPE="externaloidcprovider.tenant.assign";
+export const EXTERNAL_OIDC_PROVIDER_TENANT_REMOVE_SCOPE="externaloidcprovider.tenant.remove";
+
+
+// These are for the exclusive use of the application and cannot be changed. The names
+// are globally unique (as are ALL scope names)
+export const ALL_INTERNAL_SCOPE_NAMES = [
+    // Tenant
+    TENANT_CREATE_SCOPE, TENANT_UPDATE_SCOPE, TENANT_DELETE_SCOPE, TENANT_READ_SCOPE,
+    // Client
+    CLIENT_CREATE_SCOPE, CLIENT_UPDATE_SCOPE, CLIENT_DELETE_SCOPE, CLIENT_READ_SCOPE,
+    // Scope itself
+    SCOPE_CREATE_SCOPE, SCOPE_UPDATE_SCOPE, SCOPE_DELETE_SCOPE, SCOPE_READ_SCOPE, SCOPE_TENANT_ASSIGN_SCOPE, SCOPE_TENANT_REMOVE_SCOPE, SCOPE_CLIENT_ASSIGN_SCOPE, SCOPE_CLIENT_REMOVE_SCOPE,
+    // Signing Keys
+    KEY_CREATE_SCOPE, KEY_DELETE_SCOPE, KEY_READ_SCOPE,
+    // Rate limits
+    RATE_LIMIT_CREATE_SCOPE, RATE_LIMIT_UPDATE_SCOPE, RATE_LIMIT_DELETE_SCOPE, RATE_LIMIT_READ_SCOPE, RATE_LIMIT_TENANT_ASSIGN_SCOPE, RATE_LIMIT_TENANT_REMOVE_SCOPE, RATE_LIMIT_TENANT_UPDATE_SCOPE,
+    // Login groups
+    LOGIN_GROUP_CREATE_SCOPE, LOGIN_GROUP_UPDATE_SCOPE, LOGIN_GROUP_DELETE_SCOPE, LOGIN_GROUP_READ_SCOPE, LOGIN_GROUP_CLIENT_ASSIGN_SCOPE, LOGIN_GROUP_CLIENT_REMOVE_SCOPE,
+    // Groups
+    GROUP_CREATE_SCOPE, GROUP_UPDATE_SCOPE, GROUP_DELETE_SCOPE, GROUP_READ_SCOPE, GROUP_USER_ASSIGN_SCOPE, GROUP_USER_REMOVE_SCOPE,
+    // Users
+    USER_CREATE_SCOPE, USER_UPDATE_SCOPE, USER_DELETE_SCOPE, USER_READ_SCOPE,
+    // External OIDC providers
+    EXTERNAL_OIDC_PROVIDER_CREATE_SCOPE, EXTERNAL_OIDC_PROVIDER_UPDATE_SCOPE, EXTERNAL_OIDC_PROVIDER_DELETE_SCOPE, EXTERNAL_OIDC_PROVIDER_READ_SCOPE, EXTERNAL_OIDC_PROVIDER_TENANT_ASSIGN_SCOPE, EXTERNAL_OIDC_PROVIDER_TENANT_REMOVE_SCOPE
+];
+
+
+
+// These are scope values which CANNOT be assigned to any tenant or client OUTSIDE of the Root Tenant
+export const ROOT_TENANT_EXCLUSIVE_INTERNAL_SCOPE_NAMES = [
+    // Tenant
+    TENANT_CREATE_SCOPE, TENANT_UPDATE_SCOPE, TENANT_DELETE_SCOPE, 
+    // Scope itself
+    SCOPE_CREATE_SCOPE, SCOPE_UPDATE_SCOPE, SCOPE_DELETE_SCOPE, SCOPE_TENANT_ASSIGN_SCOPE, SCOPE_TENANT_REMOVE_SCOPE,
+    // Rate limits
+    RATE_LIMIT_CREATE_SCOPE, RATE_LIMIT_UPDATE_SCOPE, RATE_LIMIT_DELETE_SCOPE, RATE_LIMIT_TENANT_ASSIGN_SCOPE, RATE_LIMIT_TENANT_REMOVE_SCOPE, RATE_LIMIT_TENANT_UPDATE_SCOPE,
+    // External OIDC providers
+    EXTERNAL_OIDC_PROVIDER_CREATE_SCOPE, EXTERNAL_OIDC_PROVIDER_UPDATE_SCOPE, EXTERNAL_OIDC_PROVIDER_DELETE_SCOPE, EXTERNAL_OIDC_PROVIDER_TENANT_ASSIGN_SCOPE, EXTERNAL_OIDC_PROVIDER_TENANT_REMOVE_SCOPE
+];
+
+// These are the scope values which can be used WITHIN a tenant. So actions such as 
+// creating/updating/deleting a group, creating/updating/deleting a client, etc.
+// This does not apply to the Root Tenant, where any scope values assigned to a group
+// in the Root Tenant apply both to the Root Tenant AND to all other Tenants.
+// These are the minimum default scope names that will be applied to EVERY Tenant that
+// is created. The admin who is creating the tenant will also be able to apply any other
+// scope values that the various applications support, and can remove some of these 
+// if they want.
+export const DEFAULT_TENANT_BOUND_INTERNAL_SCOPE_NAMES = [
+    // Tenant
+    TENANT_READ_SCOPE,
+    // Client
+    CLIENT_CREATE_SCOPE, CLIENT_UPDATE_SCOPE, CLIENT_DELETE_SCOPE, CLIENT_READ_SCOPE,
+    // Scope itself
+    SCOPE_READ_SCOPE, SCOPE_CLIENT_ASSIGN_SCOPE, SCOPE_CLIENT_REMOVE_SCOPE,
+    // Signing keys
+    KEY_CREATE_SCOPE, KEY_DELETE_SCOPE, KEY_READ_SCOPE,
+    // Rate limits
+    RATE_LIMIT_READ_SCOPE,
+    // Login groups
+    LOGIN_GROUP_CREATE_SCOPE, LOGIN_GROUP_UPDATE_SCOPE, LOGIN_GROUP_DELETE_SCOPE, LOGIN_GROUP_READ_SCOPE, LOGIN_GROUP_CLIENT_ASSIGN_SCOPE, LOGIN_GROUP_CLIENT_REMOVE_SCOPE,
+    // Groups
+    GROUP_CREATE_SCOPE, GROUP_UPDATE_SCOPE, GROUP_DELETE_SCOPE, GROUP_READ_SCOPE, GROUP_USER_ASSIGN_SCOPE, GROUP_USER_REMOVE_SCOPE,
+    // Users
+    USER_CREATE_SCOPE, USER_UPDATE_SCOPE, USER_DELETE_SCOPE, USER_READ_SCOPE,
+    // External OIDC providers
+    EXTERNAL_OIDC_PROVIDER_READ_SCOPE
+];
+
+
+// ************************************************************************** //
+// 
+//                  TENANT-RELATED CONSTANTS
+// 
+// ************************************************************************** //
+// Globally unique group names
+export const ROOT_TENANT_ADMIN_GROUP_NAME="Root Tenant Admin Group";
+export const ROOT_TENANT_VIEW_GROUP_NAME="Root Tenant View Group";
+
+export const GLOBALLY_UNIQUE_GROUP_NAMES = [
+    ROOT_TENANT_ADMIN_GROUP_NAME,
+    ROOT_TENANT_VIEW_GROUP_NAME
+];
+
+export const ROOT_TENANT_ADMIN_GROUP_SCOPE_NAMES = ALL_INTERNAL_SCOPE_NAMES;
+
+export const ROOT_TENANT_VIEW_GROUP_SCOPE_NAMES = [
+    TENANT_READ_SCOPE,
+    CLIENT_READ_SCOPE,
+    SCOPE_READ_SCOPE,
+    KEY_READ_SCOPE,
+    RATE_LIMIT_READ_SCOPE,
+    LOGIN_GROUP_READ_SCOPE,
+    GROUP_READ_SCOPE,
+    USER_READ_SCOPE,
+    EXTERNAL_OIDC_PROVIDER_READ_SCOPE
+];
+
+
+// ************************************************************************** //
+// 
+//                  IDENTITY-RELATED CONSTANTS
+// 
+// ************************************************************************** //
+export const PASSWORD_HASHING_ALGORITHM_SHA_256_64K_ITERATIONS="password-hash-sha-256-64k-iterations";
+export const PASSWORD_HASHING_ALGORITHM_SHA_256_128K_ITERATIONS="password-hash-sha-256-128k-iterations";
+export const PASSWORD_HASHING_ALGORITHM_BCRYPT_10_ROUNDS="password-hash-bcrypt-10-rounds";
+export const PASSWORD_HASHING_ALGORITHM_BCRYPT_12_ROUNDS="password-hash-bcrypt-12-rounds";
+export const PASSWORD_HASHING_ALGORITHM_PBKDF2_10000_ITERATIONS="password-hash-pbkdf2-10000-iterations";
+export const PASSWORD_HASHING_ALGORITHM_PBKDF2_20000_ITERATIONS="password-hash-pbkdf2-20000-iterations";
+// 96 byte salt (minimum recommended by NIST is 32)
+export const PASSWORD_SALT_LENGTH=96; 
+export const PASSWORD_MINIMUM_LENGTH=10;
+export const PASSWORD_MAXIMUM_LENGTH=64;
